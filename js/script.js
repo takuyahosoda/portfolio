60e063d4b35b19076888835411144058bce3a541
takuyahosoda <takuya.hosoda314@gmail.com>
2022-07-24 13:58:04 +0900

    マークアップ修正

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
 const swiper = new Swiper(".swiper", {
   slidesPerView: 1.5,
   centeredSlides: true,
@@ -16,14 +20,16 @@ const swiper = new Swiper(".swiper", {
 
 // スムーススクロール
 const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
-  for (let i = 0; i < smoothScrollTrigger.length; i++){
+for (let i = 0; i < smoothScrollTrigger.length; i++){
     smoothScrollTrigger[i].addEventListener('click', (e) => {
       e.preventDefault();
       let href = smoothScrollTrigger[i].getAttribute('href');
 			let targetElement = document.getElementById(href.replace('#', ''));
       const rect = targetElement.getBoundingClientRect().top;
       const offset = window.pageYOffset;
-      const target = rect + offset - 20;
+      const adjust = 100;
+
+      const target = rect + offset + adjust;
       window.scrollTo({
         top: target,
         behavior: 'smooth',
@@ -33,14 +39,38 @@ const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
 
 const topLink = document.getElementById('js_top');
 const header = document.getElementById('js_header').offsetHeight;
+const animationElement = document.querySelectorAll('.ly_container');
+const maskItem = document.querySelectorAll('.mask');
+
+topLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
 
 window.addEventListener('scroll', () => {
 	const scroll = window.pageYOffset || document.documentElement.scrollTop;
 	const windowHeight = window.innerHeight; // 現在のブラウザの高さ
 
+  for (let i = 0; i < animationElement.length; i++) {
+    const animationTarget = animationElement[i].getBoundingClientRect().top + 200;
+    if (windowHeight > animationTarget) {
+      animationElement[i].classList.add('active');
+    }
+  }
+
+  for (let i = 0; i < maskItem.length; i++) {
+    const maskTarget = maskItem[i].getBoundingClientRect().top + maskItem[i].clientHeight * 0.5;
+    if (windowHeight > maskTarget) {
+      maskItem[i].classList.add('active');
+    }
+  }
+
 	if (header < scroll) {
 		topLink.classList.add('show');
 	} else {
 		topLink.classList.remove('show');
-	}
+  }
 });
